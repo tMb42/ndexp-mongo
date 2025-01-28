77ec3a39c274d7dea366bbd7ebfc53ef7581d558
tMb42 <trymybest4245@gmail.com>
2025-01-28 23:31:03 +0530

    Email Varification fixed

--- services/emailVerificationService.js
+++ services/emailVerificationService.js
@@ -10,7 +10,7 @@ const UserResource = require('../resources/UserResource');
 const mailTransport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: false, // Use `true` for port 465, `false` for all other ports
+  secure: true, // Use `true` for port 465, `false` for all other ports
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
